92f4caa67b360d9ec094cec1cdde6755dc1ce3b0
Gabriel Rodrigues <garabrielra@gmail.com>
2024-07-03 17:19:50 -0300

    criando app

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Home1')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
