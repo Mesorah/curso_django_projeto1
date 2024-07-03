d26d91661c5cf82a0fd762b60fe534d82a4738ab
Gabriel Rodrigues <garabrielra@gmail.com>
2024-07-03 18:06:23 -0300

    template

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home1')
+    return render(request, 'recipes/home.html', context={'name': 'Gabriel Rodrigues'})
 
 def sobre(request):
     return HttpResponse('Sobre')
