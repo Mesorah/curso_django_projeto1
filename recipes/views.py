167cdc64e5cd8bd6c798857befa14398f0929e91
Gabriel Rodrigues <garabrielra@gmail.com>
2024-07-09 16:46:50 -0300

    getlistor404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -10,9 +10,15 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id,
+          is_published=True)
+          .order_by('-id'))
 
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
+
+    return render(request, 'recipes/pages/category.html', context={'recipes': recipes,
+                                                                    'title': f'{recipes[0].category.name}  - Category | '
+                                                                    })
 
 
 def recipe(request, id):
