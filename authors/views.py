6830d98d829a66335307621c0d7a4aca70efe982
Gabriel Rodrigues <gabrielcambara123@gmail.com>
2024-07-23 18:13:08 -0300

    teste

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from authors.forms import RegisterForm
+from django.http import Http404
 
-# Create your views here.
+
+def register_view(request):
+    request.session['number'] = request.session.get('number') or 1
+    request.session['number'] += 1
+    form = RegisterForm()
+
+    return render(request, 'authors/pages/register_view.html', context={
+        'form': form,
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404
+
+    form = RegisterForm(request.POST)
+
+    return render(request, 'authors/pages/register_view.html', context={
+        'form': form,
+    })
