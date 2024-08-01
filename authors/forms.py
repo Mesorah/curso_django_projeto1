536ac7f749af83eb3676b68d89751bc1066da82d
Gabriel Rodrigues <gabrielcambara123@gmail.com>
2024-08-01 18:09:09 -0300

    tets

--- authors/forms.py
+++ authors/forms.py
@@ -36,8 +36,37 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Type you password')
         add_placeholder(self.fields['password2'], 'Repeat you password')
 
+    username = forms.CharField(
+        label='Username',
+        help_text=(
+            'Username must have letters, numbers or one of those @.+-_. '
+            'The length should be between 4 and 150 characters.'
+        ),
+        error_messages={
+            'required': 'This field must not be empty',
+            'min_length': 'Username must have at least 4 characters',
+            'max_length': 'Username must have less than 150 character'
+        },
+        min_length=4, max_length=150
+    )
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid',
+    )
+
     password = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
 
         error_messages={
@@ -57,8 +86,14 @@ class RegisterForm(forms.ModelForm):
 
     password2 = forms.CharField(
         required=True,
+
         widget=forms.PasswordInput(),
-        label='Password2'
+
+        label='Password2',
+
+        error_messages={
+            'required': 'Please, repeat your password'
+        },
     )
 
     class Meta:
@@ -71,22 +106,16 @@ class RegisterForm(forms.ModelForm):
             'password',
         ]
 
-        labels = {
-            'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
-        }
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
 
-        help_texts = {
-            'email': 'The e-mail must be valid'
-        }
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
 
-        error_messages = {
-            'username': {
-                'required': 'This field must not be empty',
-            }
-        }
+        return email
 
     def clean(self):
         cleaned_data = super().clean()
