80ae02de9dab3d1f4484744c3fa7409cacce7169
Messorah <gabrielrodriguesaguiar43@gmail.com>
2024-09-25 15:23:44 -0300

    functional tests

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -49,9 +49,7 @@ class RecipeHomeViewTest(RecipeTestBase):
 
     @patch('recipes.views.PER_PAGE', new=3)
     def test_recipe_home_is_paginated(self):
-        for i in range(8):
-            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
-            self.make_recipe(**kwargs)
+        self.make_recipe_in_batch(qtd=8)
 
         response = self.client.get(reverse('recipes:home'))
         recipes = response.context['recipes']
@@ -64,9 +62,7 @@ class RecipeHomeViewTest(RecipeTestBase):
 
     @patch('recipes.views.PER_PAGE', new=3)
     def test_invalid_page_query_uses_page_one(self):
-        for i in range(8):
-            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
-            self.make_recipe(**kwargs)
+        self.make_recipe_in_batch(qtd=8)
 
         response = self.client.get(reverse('recipes:home') + '?page=12A')
         self.assertEqual(
