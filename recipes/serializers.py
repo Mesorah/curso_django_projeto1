8afb16f682586ac94d295cae13d42a35d725bda0
Messorah <garabrielra@gmail.com>
2025-04-03 15:10:37 -0300

    usando modelserializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,12 +1,50 @@
 from rest_framework import serializers
 
+from recipes.models import Recipe
+from tag.models import Tag
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author',
+            'category', 'tags', 'public', 'preparation',
+            'tag_objects', 'tag_links'
+        ]
+
+    public = serializers.BooleanField(
+        source='is_published',
+        read_only=True
+    )
+    preparation = serializers.SerializerMethodField(
+        read_only=True
+    )
+    category = serializers.StringRelatedField(
+        read_only=True
+    )
+    # author = serializers.StringRelatedField()
+    # tags = serializers.PrimaryKeyRelatedField(
+    #     queryset=Tag.objects.all(),
+    #     many=True
+    # )
+    tag_objects = TagSerializer(
+        many=True,
+        source='tags',
+        read_only=True
+    )
+    tag_links = serializers.HyperlinkedRelatedField(
+        many=True,
+        source='tags',
+        view_name='recipes:recipes_api_v2_tag',
+        read_only=True
+    )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
